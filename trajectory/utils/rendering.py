781517dfaedba82ea69a1da23a16f33dd16821cb
Ethan Brooks <ethanabrooks@gmail.com>
2023-04-07 14:59:44 -0400

    improve rendering

--- trajectory/utils/rendering.py
+++ trajectory/utils/rendering.py
@@ -140,7 +140,7 @@ class Renderer:
         if type(dim) == int:
             dim = (dim, dim)
 
-        self.viewer.render(*dim)
+        self.viewer.render(*dim, camera_id=-1)
         data = self.viewer.read_pixels(*dim, depth=False)
         data = data[::-1, :, :]
         return data
@@ -212,7 +212,7 @@ class PointRenderer:
             actions = np.diff(states, axis=0)
 
         for state, action in zip(states, actions):
-            ax.arrow(*state, *action)
+            ax.arrow(*state, *(0.2 * action))
 
         plt.savefig(savepath + ".png")
         if wandb.run is not None:
