0b5e9a38c1a3fc29cb7b9dd0b7a9f2512605b796
Ethan Brooks <ethanabrooks@gmail.com>
2023-04-20 14:56:57 -0400

    [multi-replay-buffers, single-task-per-buffer] load SparsePointEnv dataset from SparsePointEnv-v0-multitask:v11

--- config/offline.py
+++ config/offline.py
@@ -147,7 +147,7 @@ SparsePointEnv_v0 = {
 TaskAwareSparsePointEnv_v0 = {
     "plan": {
         "renderer": "PointRenderer",
-        "loadpath": "rldl/In-Context Model-Based Planning/f6qqcr78",
+        "loadpath": "rldl/In-Context Model-Based Planning/mr3z0lb4",
     },
 }
 
