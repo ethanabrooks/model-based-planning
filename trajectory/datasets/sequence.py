b55f688782585a9d357fc9c5e085b67fb5c88d07
ethanabrooks <ethanabrooks@gmail.com>
2023-04-25 17:39:44 -0400

    add optimized method for computing indices

--- trajectory/datasets/sequence.py
+++ trajectory/datasets/sequence.py
@@ -153,6 +153,7 @@ class SequenceDataset(torch.utils.data.Dataset):
 
         ## get valid indices
         indices = []
+        indices2 = []
         for path_ind, length in enumerate(
             tqdm(self.path_lengths, desc="Assign indices")
         ):
@@ -162,7 +163,15 @@ class SequenceDataset(torch.utils.data.Dataset):
             for i in range(end):
                 indices.append((path_ind, i, i + sequence_length))
 
+            starts = np.arange(1 - sequence_length, end)
+            ends = starts + sequence_length
+            idxs = path_ind * np.ones_like(starts)
+            starts = np.clip(starts, 0, None)
+            indices2.append(np.stack([idxs, starts, ends]))
+
         self.indices = np.array(indices)
+        self.indices2 = np.concatenate(indices2, axis=1).T
+        assert np.all(np.sort(self.indices2, axis=0) == np.sort(self.indices, axis=0))
         self.observation_dim = observations.shape[1]
         self.action_dim = actions.shape[1]
         self.joined_dim = self.joined_raw.shape[1]
