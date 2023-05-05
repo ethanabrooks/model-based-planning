b3a4f1e7c948ccf739eef7fcfeaf61cfa2b4bec2
ethanabrooks <ethanabrooks@gmail.com>
2023-05-05 09:37:31 -0400

    fix video logging by adding save_directory

--- trajectory/utils/video.py
+++ trajectory/utils/video.py
@@ -15,10 +15,6 @@ def save_video(filename, video_frames, fps=60, video_format="mp4"):
     assert fps == int(fps), fps
 
     _make_dir(filename)
-    if wandb.run is not None:
-        (time, height, width, channel) = [i for i, _ in enumerate(video_frames.shape)]
-        data = np.transpose(video_frames, (time, channel, height, width))
-        wandb.log({filename: wandb.Video(data, fps=fps, format=video_format)})
     skvideo.io.vwrite(
         filename,
         video_frames,
@@ -30,6 +26,10 @@ def save_video(filename, video_frames, fps=60, video_format="mp4"):
             "-pix_fmt": "yuv420p",  # '-pix_fmt=yuv420p' needed for osx https://github.com/scikit-video/scikit-video/issues/74
         },
     )
+    if wandb.run is not None:
+        (time, height, width, channel) = [i for i, _ in enumerate(video_frames.shape)]
+        data = np.transpose(video_frames, (time, channel, height, width))
+        wandb.log({filename: wandb.Video(data, fps=fps, format=video_format)})
 
 
 def save_videos(filename, *video_frames, **kwargs):
