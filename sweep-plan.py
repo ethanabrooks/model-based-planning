4cbc3f09e7ccc5007bad4e8cdef5c4e750f3cdd6
ethanabrooks <ethanabrooks@gmail.com>
2023-05-06 04:26:16 -0400

    increase sweep size for sweep-plan

--- sweep-plan.py
+++ sweep-plan.py
@@ -1,6 +1,6 @@
+import torch
 from ray import tune
 
 from plan import sweep
-import torch
 
-sweep(seed=tune.grid_search(list(range(torch.cuda.device_count()))))
+sweep(seed=tune.grid_search(list(range(16))))
